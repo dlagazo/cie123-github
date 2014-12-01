6aafabdb57bc3a78551e004f8877aa28c739881f
Harry Bo <harrybo@ateneoinnovation.org>
2014-12-01 16:05:57 +0800

    yo~

--- CIE123/src/HBoClass.java
+++ CIE123/src/HBoClass.java
@@ -1,9 +1,11 @@
-//Developer: Harry Bo
+/* Developer: Harry Bo
+ * 
+ * 
+ */
 
 
 public class HBoClass {
 	public static void main(String []args){
 		System.out.println("NALULUHAAN MO KATARATA KO!!!!!");
 	}
-	
 }
