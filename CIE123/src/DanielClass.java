c072c99ae283adbae7b657a2a47e13755a19ed22
Hadrian Paulo Lim <hadrianpaulo@gmail.com>
2014-12-03 04:08:14 +0800

    Added alpha javadocs for HadrianClass

--- CIE123/src/DanielClass.java
+++ CIE123/src/DanielClass.java
@@ -1,4 +1,5 @@
 
+
 public class DanielClass {
 	public static void main(String[] args)
 	{
--- CIE123/src/HadrianClass.java
+++ CIE123/src/HadrianClass.java
@@ -1,4 +1,55 @@
+/*
+ * @name Hadrian Paulo Lim 
+ * @since 12-03-2014
+ * @version branch-hadrianpaulo version 0.5
+ * 
+ */
+
 
 public class HadrianClass {
+	/*
+	 * Hadrian Class for primarily constructing
+	 * <b> Hadrian-based </b> classes only.
+	 * 
+	 */
+	public HadrianClass(String UselessParam){
+		/*
+		 * @param UselessParam
+		 * This doesn't do anythiiing
+		 * @return just prints something :))
+		 */
+		System.out.print("Created a copy of Hadrian");
+	}
+	public void lol(){
+		/*
+		 * @return just prints lol :))
+		 * 
+		 */
+		System.out.print("lol");
+	}
+	public int random(int start, int end){
+		/*
+		 * @param start starting range
+		 * @param end ending range
+		 * @result isn't actually random Fool your peers!
+		 * 
+		 */
+		return 2;
+	}
 	
+	public long meh(){
+		/*
+		 * @result just gives you a 23143213 of type long
+		 */
+		return 23143213;
+	}
+	
+	public void whoami(){
+		/*
+		 * @result see for yourself!
+		 */
+		
+		System.out.print("Guess who.");
+	}	
 }
+	
