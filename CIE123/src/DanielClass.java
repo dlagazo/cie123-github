28f17641e36edf11c5217da7124b287c72fb02ec
Dawn Corpuz <corpuzdawn@gmail.com>
2014-12-03 16:29:20 +0800

    Documentation

--- CIE123/src/DanielClass.java
+++ CIE123/src/DanielClass.java
@@ -1,7 +1,26 @@
-
+/*
+ * @author corpuzdawn
+ * @since 2014-12-3
+ * @version branch-corpuzdawn. version 1.0
+ */
 public class DanielClass {
+	/*
+	 * This class is called DanielClass. This
+	 * is a description for the class
+	 * <b>This is in bold</b>
+	 * <h1>This is header</h1>
+	 */
 	public static void main(String[] args)
 	{
 		
+	}
+	/*
+	 * <b> This function accepts two parameters
+	 * @param arg1 This is a string
+	 * @param arg2 This is an int
+	 * @return This always returns 5.
+	 */
+	public static int someMethod(String arg1, int arg2){
+		return 5;
 	}
 }
