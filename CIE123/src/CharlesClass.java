34f6e5eec0a7452304ab461b7b41d97d0e908e01
Charles Lee <charles26lee@gmail.com>
2014-12-03 16:23:02 +0800

    added add, multiple, subtract, negate, and square functions

--- CIE123/src/CharlesClass.java
+++ CIE123/src/CharlesClass.java
@@ -1,4 +1,28 @@
-
+/*
+ * JavaDoc Test
+ */
 public class CharlesClass {
-//-.-
+	public static void main(String[] args){
+		int x = 3, y = 4;
+		System.out.println(add(x, y));
+		y=negate(y);
+		System.out.println(multiply(x, y));
+		x=square(x);
+		System.out.println(subtract(x, y));
+	}
+	static int add(int a, int b){
+		return a+b;
+	}
+	static int square(int a){
+		return a*a;
+	}
+	static int multiply(int a, int b){
+		return a*b;
+	}
+	static int subtract(int a, int b){
+		return a-b;
+	}
+	static int negate(int a){
+		return -a;
+	}
 }
