42e4f422ad2c6c009ceb448387d3c0029173fc05
Harry Bo <harrybo@ateneoinnovation.org>
2014-12-03 16:23:43 +0800

    With java doc

--- CIE123/src/IvanClass1.java
+++ CIE123/src/IvanClass1.java
@@ -1,6 +1,51 @@
-
+/**
+ * This is the Ivan Class
+ * @author Ivan
+ *
+ */
 public class IvanClass1 {
-
-	Test1
-	
+/**
+ * 
+ * @param i
+ * @return
+ */
+	int startCount (int i){
+		i++;
+		return i; 
+	}
+	/**
+	 * multipies
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+    double multiply(double x, double y){
+    	return x*y;
+    }
+    /**
+     * prints anything
+     * @param a
+     */
+    void print(String a){
+    	System.out.println(a);
+    }
+    /**
+     * gets the difference
+     * @param x
+     * @param y
+     * @return
+     */
+    double subtract(double x, double y){
+    	return x-y;
+    }
+    /**
+     * gets the sum
+     * @param x
+     * @param y
+     * @return
+     */
+    double add(double x, double y){
+    	return x+y;
+    }
 }
+
