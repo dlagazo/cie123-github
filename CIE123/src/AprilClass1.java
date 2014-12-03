6fa17b3c6b2b027b0792c905368e87b7d9748d87
apriljewel <april.domingo@obf.ateneo.edu>
2014-12-03 16:22:36 +0800

    Javadoc version 1.0

--- CIE123/src/AprilClass1.java
+++ CIE123/src/AprilClass1.java
@@ -1,4 +1,41 @@
+/**
+ * @author - April
+ * @since - 2014/12/3
+ * @version - branch-apriljewel Version 1.0
+ */
 
 public class AprilClass1 {
+	/**This class is called DanielClass.
+	*This is the class description.
+	*<b>This is in bold</b>
+	*<h1>This is header1</h1>
+	*@param args Some parameter
+	*/
+	public static void main(String[] args){}
+	/**
+	 * <b>This function accepts two parameters</b>
+	 * @param arg1 This is a String
+	 * @param arg2 This is an int
+	 * @return This always returns 5.
+	 */
+}
 
+public static add (int a, int b) {
+	return a+b;
+}
+
+public static sub (int a, int b) {
+	return a-b;
+}
+
+public static mul (int a, int b) {
+	return a*b;
+}
+
+public static div (int a, int b) {
+	return a/b;
+}
+
+public static copy (int a) {
+	return a;
 }
