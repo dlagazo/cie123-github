aee58822eefc0f5168c6e30d83d59ebfafbb4887
Glenn Harvey Liwanag <solitude02@server.fake>
2014-12-03 15:54:15 +0800

    Added documentation

--- CIE123/src/GlennClass1.java
+++ CIE123/src/GlennClass1.java
@@ -1,5 +1,29 @@
 /**
- * Created by solitude on 12/1/14.
+ * @author Glenn Harvey Liwanag
+ * @since 2014-12-03
+ * @version branch-solitude02, version 1.0
  */
 public class GlennClass1 {
+    /**
+     * This class is called GlennClass1. This
+     * is a description for the class.
+     * <b>This is in bold.</b>
+     * <h1>This is header1.</h1>
+     **
+     *
+     *
+     */
+    public static void main(String[] args) {
+        /**
+         * This is the main function
+         * @param arg1 This is a String
+         * @param arg2 This is an int
+         * @return This does not return a value
+         */
+    }
+
+    public static int someMethod(String arg1, int arg2) {
+        return 5;
+    }
+
 }
