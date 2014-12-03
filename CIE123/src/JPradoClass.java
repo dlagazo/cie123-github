0156ec481e7df9c871853737d8233b8ab6757fe0
Jay-Ar Prado <jayyarr.shirousa@yahoo.com>
2014-12-03 16:18:46 +0800

    Javadoc exercise!

--- CIE123/src/JPradoClass.java
+++ CIE123/src/JPradoClass.java
@@ -1,5 +1,84 @@
-//Developer: Jay-Ar Prado
+/**
+ * @author Jay-Ar Prado
+ * @since 2014-12-7
+ * @version branch-jayarprado, version 1.0
+ * <br>This is called JPradoClass. This
+ * is a description for the class.
+ * <b>This is in bold.</b>
+ * <h1>And this is header1!</h1>
+ * Yay!
+ */
 
 public class JPradoClass {
+	
+	/**
+	 * This is the <b>main</b> function. This
+	 * is a description for this method.
+	 */
+	public static void main(String[] args) {
+	}
+	
+	/**
+	 * This is called <b>function 1</b>. This
+	 * is a description for this method.
+	 * This functions accepts two parameters
+	 * and doesn't return anything.
+	 * @param arg1 a String input
+	 * @param arg2 an int input
+	 */
+	public static void function1(String arg1, int arg2) {
+	}
+	
+	/**
+	 * This is called <b>function 2</b>. This
+	 * is a description for this method.
+	 * This functions accepts two parameters
+	 * and returns a double.
+	 * @param arg1 a String input
+	 * @param arg2 an int input
+	 * @return int 0
+	 */
+	public static int function2(String arg1, int arg2) {
+		return 0;
+	}
+	
+	/**
+	 * This is called <b>function 3</b>. This
+	 * is a description for this method.
+	 * This functions accepts two parameters
+	 * and returns a double.
+	 * @param arg1 a String input
+	 * @param arg2 an int input
+	 * @return boolean true
+	 */
+	public static boolean function3(String arg1, int arg2) {
+		return true;
+	}
+	
+	/**
+	 * This is called <b>function 5</b>. This
+	 * is a description for this method.
+	 * This functions accepts two parameters
+	 * and returns a double.
+	 * @param arg1 a String input
+	 * @param arg2 an int input
+	 * @return char 'a'
+	 */
+	public static char function4(String arg1, int arg2) {
+		return 'a';
+	}
+	
+	/**
+	 * This is called <b>function 5</b>. This
+	 * is a description for this method.
+	 * This functions accepts two parameters
+	 * and returns a double.
+	 * @param arg1 a String input
+	 * @param arg2 an int input
+	 * @return double 1.25
+	 */
+	public static double function5(String arg1, int arg2) {
+		return 1.25;
+	}
 
 }
