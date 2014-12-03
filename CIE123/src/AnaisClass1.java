442808782bb9649b9acd86ca535130cba8a28171
AnaisRatier <ratier.anais@gmail.com>
2014-12-03 16:23:51 +0800

    We did our first JavaDoc

--- CIE123/src/AnaisClass1.java
+++ CIE123/src/AnaisClass1.java
@@ -1,6 +1,59 @@
-
+/**
+ * 
+ * @author Anais Ratier
+ * @since 2014-12-3
+ * @version Branch Anais, version 1.0
+ */
 public class AnaisClass1 {
-	hello
-	world
-	java
+	/**
+	 * This class is AnaisRatierClass1
+	 * <b>This is in bold</b>
+	 * <h1>This is header 1</h1>
+	 */
+	
+	public void main(){
+		System.out.println("Hello!");
+		Function();
+		}
+	/**
+	 * This is a string function
+	 * @return
+	 */
+	public String Function (){
+		return"Hello";	
+	}
+	/**
+	 * This is a double function
+	 * @return
+	 */
+	public double Function2 (){
+		return 2+2;
+	}
+	/**
+	 * This is a double function with parameters
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public double Function3 (double x, double y){
+		return x*y;
+	}
+	/**
+	 * This is a int function
+	 * @param i
+	 * @return
+	 */
+	public int Function4 (int i){
+		return i;
+	}
+	/**
+	 * This is a char function
+	 * @param l
+	 * @return 1
+	 */
+	public char Function5(char l){
+		return (l);
+	}
+	
 }
+
