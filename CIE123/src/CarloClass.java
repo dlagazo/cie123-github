59bdda542e289dca4a388948039d60e74f02969f
carlo029 <antioquia_carlo2006@yahoo.com>
2014-12-03 16:26:09 +0800

    Java class with documentation

--- CIE123/src/CarloClass.java
+++ CIE123/src/CarloClass.java
@@ -1,4 +1,55 @@
-
+/**
+ * 
+ * @author Carlo
+ *@since 2014-December-3
+ *@version branch-carlo029, version 1.0
+ */
 public class CarloClass {
+	//push cat
+	//hi
+	
+	/**
+	 * This class is called CarloClass
+	 */
+	public static void display()
+	{
+		/**
+		 * this function displays the word Hello on the console
+		 */
+		System.out.println("Hello");
+	}
+	
+	public static void name(String msg)
+	{
+		/**
+		 * this function displays a user input string 
+		 */
+		System.out.println(msg);
+	}
+	
+	public static void monitor()
+	{
+		/**
+		 * this function outputs the message "the monitor is wide"
+		 */
+		System.out.println("The monitor is wide");
+	}
+	
+	public static void hi()
+	{
+		/**
+		 * This function displays a message that tells the user to stop using the computer
+		 */
+		System.out.println("Hi, please stop using this computer");
+	}
+	
+	public static void asdf123()
+	{
+		/**
+		 * This function displays a spam message
+		 */
+		System.out.println("this function doesn't really do anything");
+	}
+	
 
 }
