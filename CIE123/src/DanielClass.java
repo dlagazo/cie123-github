2ed14b63425ccc1c71775fbf0ac44802f78e048c
Ramon Imbao <ramonimbao@gmail.com>
2014-12-03 16:17:15 +0800

    Added some javadocs and new functions! :)

--- CIE123/src/DanielClass.java
+++ CIE123/src/DanielClass.java
@@ -1,7 +1,30 @@
+/**
+ * @author ramonimbao
+ * @since 2014-12-3
+ * @version branch-ramonimbao, version 1.0
+ */
 
+
+/**
+ * This class is called DanielCalss. This
+ * is a description for the class.
+ * <b>This is in bold</b>
+ * <h1>This is header1</h1>
+ */
 public class DanielClass {
+	
 	public static void main(String[] args)
 	{
 		
+	}
+	
+	/**
+	 * <b>This function accepts two parameters.</b>
+	 * @param arg1 This is a string
+	 * @param arg2 This is an int
+	 * @return This always returns 5.
+	 */
+	public static int someMethod(String arg1, int arg2) {
+		return 5;
 	}
 }
--- CIE123/src/RamonClass.java
+++ CIE123/src/RamonClass.java
@@ -1,4 +1,70 @@
+/**
+ * @author ramonimbao
+ * @since 2014-12-3
+ * @version branch-ramonimbao, version 1.0
+ */
 
+
+/**
+ * This class is called RamonClass. This
+ * is a description of the class.
+ * <marquee><h1>Oh no!</h1></marquee>
+ */
 public class RamonClass {
-
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * <b>This function lets Ramon eat.</b>
+	 * @param foods The foods that you'd feed Ramon
+	 */
+	public static void eat(String[] foods) {
+		for(int i=0; i<foods.length; i++) {
+			System.out.println("Eating " + foods + "...");
+		}
+	}
+		
+	/**
+	 * <b>This function lets Ramon poop.</b>
+	 */
+	public static void poop() {
+		System.out.println("Pooping...");
+	}
+	
+	/**
+	 * <b>This function lets Ramon sleep for 
+	 * a set number of hours, minutes, and
+	 * seconds.</b>
+	 * @param hours The number of hours Ramon will sleep.
+	 * @param minutes The number of minutes Ramon will sleep.
+	 * @param seconds The number of seconds Ramon will sleep.
+	 */
+	public static void sleep(long hours, long minutes, long seconds) {
+		System.out.println("Sleeping for " + hours + "h " + minutes + "m " + seconds + "s...");
+	}
+	
+	
+	/**
+	 * <b>This function will make Ramon add two
+	 * numbers x and y for no reason at all.</b>
+	 * @param x The first number
+	 * @param y The second number
+	 * @return The sum of the two numbers
+	 */
+	public static float addForNoReason(float x, float y) {
+		return x + y;
+	}
+	
+	
+	/**
+	 * <b>This function will make Ramon spend an
+	 * inaccurate amount of money due to how <i>float</i>s
+	 * work in computers.</b>
+	 */
+	public static void spendInaccurately(float amount) {
+		System.out.println("Spending Php" + amount + "...");
+	}
+	
 }
