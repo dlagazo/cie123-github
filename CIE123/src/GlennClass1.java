a07eb97c420e1e149dedf77de064335d35703d79
Glenn Harvey Liwanag <solitude02@server.fake>
2014-12-03 16:07:45 +0800

    Added five getter functions + respective documentation

--- CIE123/src/GlennClass1.java
+++ CIE123/src/GlennClass1.java
@@ -16,15 +16,54 @@ public class GlennClass1 {
     public static void main(String[] args) {
 
     }
-    
+
     /**
-     * This is the main function
+     * This is someMethod()
      * @param arg1 This is a String
      * @param arg2 This is an int
-     * @return This does not return a value
+     * @return This always return 5
      */
     public static int someMethod(String arg1, int arg2) {
         return 5;
     }
 
+    /**
+     * This method returns the name of the object
+     * @return Glenn Harvey Liwanag
+     */
+    public static String getMyName() {
+        return "Glenn Harvey Liwanag";
+    }
+
+    /**
+     * This method returns the partner's name of the object
+     * @return Angela Magnaye
+     */
+    public static String getPartnerName() {
+        return "Angela Magnaye";
+    }
+
+    /**
+     * This method returns the professor's name of the object
+     * @return Daniel Lagazo
+     */
+    public static String getProfessorName() {
+        return "Daniel Lagazo";
+    }
+
+    /**
+     * This method returns the age of the object
+     * @return 19
+     */
+    public static int getMyAge() {
+        return 19;
+    }
+
+    /**
+     * This method returns the partner's age of the object
+     * @return 22
+     */
+    public static int getPartnerAge() {
+        return 22;
+    }
 }
